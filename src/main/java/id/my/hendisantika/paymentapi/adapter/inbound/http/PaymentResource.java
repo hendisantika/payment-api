4d03fbb9415ed9f5db6a59d41ece1c05d22b1154
hendisantika <hendisantika@yahoo.co.id>
2026-03-15 08:07:12 +0700

    feat: add createPayment in PaymentResource 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/paymentapi/adapter/inbound/http/PaymentResource.java
+++ src/main/java/id/my/hendisantika/paymentapi/adapter/inbound/http/PaymentResource.java
@@ -1,7 +1,14 @@
 package id.my.hendisantika.paymentapi.adapter.inbound.http;
 
 import ch.qos.logback.classic.Logger;
+import id.my.hendisantika.paymentapi.adapter.dto.PaymentDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +32,33 @@ public class PaymentResource {
     Logger logger = Logger.getLogger(PaymentResource.class.getName());
 
     public PaymentResource(CreatePaymentEventUseCase createPaymentEventUseCase) {
-
         this.createPaymentEventUseCase = createPaymentEventUseCase;
+    }
+
+    @Operation(summary = "Create a new payment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Payment created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request")
+    })
+    @PostMapping
+    public ResponseEntity<String> createPayment(@RequestBody PaymentDto paymentDto) {
+        //This API is only to receive the payment request and publish the event, the actual processing of the payment will be done by another service that will consume the event from Kafka.
+        //The validation of the payment will be done by the consumer service, this API will only validate the request body and publish the event to Kafka.
+        logger.info("Resource rest: " + paymentDto.getCustomerId()+ paymentDto.getAmount()+ paymentDto.getOrderId());
+
+        // Convert PaymentDto to PaymentEvent witch one the event that will be published to Kafka, this is done to decouple the API from the domain model, so we can change the domain model without affecting the API and vice versa.
+        PaymentEvent paymentEvent = new PaymentEvent(paymentDto);
+        paymentEvent.setOrderId(paymentDto.getOrderId());
+        paymentEvent.setAmount(paymentDto.getAmount());
+        paymentEvent.setCustomerId(paymentDto.getCustomerId());
+
+        logger.info("Resource rest: {}{}{}", paymentEvent.getCustomerId(), paymentEvent.getAmount(), paymentEvent.getOrderId());
+
+        // When we use hexagonal architecture we want to decouple the API from the domain model, so we use a DTO to transfer the data from the API to the domain model, and then we use a use case to handle the business logic and publish the event to Kafka.
+        createPaymentEventUseCase.createPaymentEvent(paymentEvent);
+
+        return ResponseEntity.ok("Payment created successfully");
+
     }
 
 }
