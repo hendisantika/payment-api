df78630562e416bed642dda5e5cf19f9aa208cc7
hendisantika <hendisantika@yahoo.co.id>
2026-03-15 08:17:30 +0700

    feat: add OutboxStatus 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/paymentapi/adapter/outbound/outbox/OutboxStatus.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.paymentapi.adapter.outbound.outbox;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : payment-api
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/03/26
+ * Time: 08.17
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OutboxStatus {
+    PENDING,
+    PROCESSED,
+    FAILED
+}
