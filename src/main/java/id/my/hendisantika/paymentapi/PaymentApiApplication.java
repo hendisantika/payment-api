d95855cc00d48c1a10dc18de77c0e4b739bc0d5a
hendisantika <hendisantika@yahoo.co.id>
2026-03-15 08:00:48 +0700

    feat: init project🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/paymentapi/PaymentApiApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.paymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PaymentApiApplication.class, args);
+    }
+
+}
